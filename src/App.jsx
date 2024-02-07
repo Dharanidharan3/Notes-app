3894c04a1a6487d0deba9c392623a7418e62cada
Dharanidharan3 <dharanisenthil64@gmail.com>
2024-02-07 14:53:24 +0530

    Notes-app

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Sidebar from "./Sidebar"
+import Notes from "./Notes"
+import Edit from './Edit'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Navigate } from "react-router-dom"
+import NotesContext from "./Context/NotesContext"
+function App() {
+
+  return (
+    <>
+     <div id='wrapper'>
+      <BrowserRouter>
+      <Sidebar/>
+      <Routes>
+        <Route path='/notes' element={
+        <NotesContext>
+        <Notes/>
+        </NotesContext>}/>
+        <Route path='/edit/:id' element={
+        <NotesContext>
+        <Edit/>
+        </NotesContext>}/>
+        <Route path='*' element={< Navigate to='/notes'/>}/>
+      </Routes>
+      </BrowserRouter>
+     </div>
+    </>
+  )
+}
+export default App
